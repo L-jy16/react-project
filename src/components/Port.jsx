4e3b1fd86337161523d79ae5fe33fb1cc923760e
jeong202 <144635774+L-jy16@users.noreply.github.com>
2023-11-07 14:15:22 +0900

    2023.11.07

--- src/components/Port.jsx
+++ src/components/Port.jsx
@@ -5,126 +5,126 @@ import port01 from '../assets/img/img01.jpg'
 const Port = () => {
   return (
     <section id="port">
-      <div class="port__inner">
-        <div class="port__title">
+      <div className="port__inner">
+        <div className="port__title">
           portfolio <em>포폴 작업물</em>
         </div>
-        <div class="port__wrap">
-          <div class="port__item p1">
-            <span class="num">01.</span>
+        <div className="port__wrap">
+          <div className="port__item p1">
+            <span className="num">01.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p2">
-            <span class="num">02.</span>
+          <div className="port__item p2">
+            <span className="num">02.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p3">
-            <span class="num">03.</span>
+          <div className="port__item p3">
+            <span className="num">03.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p4">
-            <span class="num">04.</span>
+          <div className="port__item p4">
+            <span className="num">04.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p5">
-            <span class="num">05.</span>
+          <div className="port__item p5">
+            <span className="num">05.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p6">
-            <span class="num">06.</span>
+          <div className="port__item p6">
+            <span className="num">06.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p7">
-            <span class="num">07.</span>
+          <div className="port__item p7">
+            <span className="num">07.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p8">
-            <span class="num">08.</span>
+          <div className="port__item p8">
+            <span className="num">08.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p9">
-            <span class="num">09.</span>
+          <div className="port__item p9">
+            <span className="num">09.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
             </p>
           </div>
-          <div class="port__item p10">
-            <span class="num">10.</span>
+          <div className="port__item p10">
+            <span className="num">10.</span>
             <a href="/">
               <img src={port01} alt="포트폴리오" />
             </a>
-            <h3 class="title">리액트를 이용한 포트폴리오</h3>
-            <p class="desc">
+            <h3 className="title">리액트를 이용한 포트폴리오</h3>
+            <p className="desc">
               이 사이트는 정말 인상적인 포트폴리오입니다.
               특히 스무스한 효과와 가로 모드로 높은 퀄리티를 자랑합니다.
               디테일과 꼼꼼함이 넘치는 포트폴리오는 개발자의 뛰어난 능력을 엿볼  수 있습니다.
